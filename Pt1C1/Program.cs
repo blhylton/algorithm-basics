32117f67c70fb3d34ea915335f146e55962f2a79
Barry Hylton <blhylton@blhylton.com>
2019-01-20 04:02:51 -0500

    Complete Part 1

---
+++ Pt1C1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Pt1C1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var tokens = input.Split(' ');
+            var a = int.Parse(tokens[0]);
+            var b = int.Parse(tokens[1]);
+            Console.WriteLine(a + b);
+        }
+    }
+}
---
+++ Pt1C2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Pt1C2
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var n = Console.ReadLine();
+            var input = Console.ReadLine();
+            if (input == null) return;
+            var tokens = input.Split(' ').Select(long.Parse).ToList();
+            if (tokens.Count != int.Parse(n)) return;
+
+            Console.WriteLine( MaxPairwiseProduct(tokens).ToString());
+        }
+
+        private static long MaxPairwiseProduct(IEnumerable<long> numbers)
+        {
+            long a = 0;
+            long b = 0;
+            foreach (var num in numbers)
+            {
+                if (num > a)
+                {
+                    if (a > b)
+                    {
+                        b = a;
+                    }
+
+                    a = num;
+                    continue;
+                }
+
+                if (num > b)
+                {
+                    b = num;
+                }
+            }
+            
+            return a * b;
+        }
+    }
+}
