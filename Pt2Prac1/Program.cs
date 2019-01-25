015655508363aabe03342b04f758908bc2d26e96
Barry Hylton <blhylton@blhylton.com>
2019-01-25 09:47:53 -0500

    Write function to compute GCD using Euclid's theorem

---
+++ Pt2Prac1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Pt2Prac1
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            if (input == null) return;
+            var inputs = input.Split(' ');
+            long[] n = { long.Parse(inputs[0]), long.Parse(inputs[1]) };
+            Console.WriteLine(EuclidGcd(n[0], n[1]));
+        }
+
+        private static long EuclidGcd(long a, long b)
+        {
+            while (true)
+            {
+                if (b == 0)
+                {
+                    return a;
+                }
+
+                var rem = a % b;
+                a = b;
+                b = rem;
+            }
+        }
+    }
+}
---
+++ Pt2Practice/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Pt2Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var n = Int64.Parse(input);
+            
+            
+        }
+    }
+}
