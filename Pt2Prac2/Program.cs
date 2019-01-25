6c29d8096c123a149be5d29d2870413bd37da4c0
Barry Hylton <blhylton@blhylton.com>
2019-01-25 09:56:22 -0500

    Write function to compute nth fibonacci term

---
+++ Pt2Prac2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Pt2Prac2
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            if (input == null) return;
+            var n = long.Parse(input);
+            Console.WriteLine(Fibonacci(n));
+        }
+
+        private static long Fibonacci(long n)
+        {
+            if(n <= 1)
+            {
+                return n;
+            }
+
+            long a = 0, b = 1;
+
+            for (var i = 2; i <= n; i++)
+            {
+                var temp = a + b;
+                a = b;
+                b = temp;
+            }
+
+            return b;
+        }
+    }
+}
