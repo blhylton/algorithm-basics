f7a14fc16ca352641c98147de8e3ed9171be5d01
Barry Hylton <blhylton@blhylton.com>
2019-01-25 11:12:33 -0500

    Find last digit of large fibonacci numbers
    Unfortunately, this doesn't work for the course because the course doesn't include an implementation of BigInteger :(

---
+++ Pt2Assignment2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Numerics;
+
+namespace Pt2Assignment2
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            if (input == null) return;
+            var n = BigInteger.Parse(input);
+            n = Fibonacci(n);
+            Console.WriteLine((n % 10));
+        }
+
+        private static BigInteger Fibonacci(BigInteger n)
+        {
+            if(n <= 1)
+            {
+                return n;
+            }
+
+            BigInteger a = 0, b = 1;
+
+            for (var i = 2; i <= n; i++)
+            {
+                var temp = a + b;
+                a = b;
+                b = temp;
+            }
+
+            return b;
+        }
+    }
+}
