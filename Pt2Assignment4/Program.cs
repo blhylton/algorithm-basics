6e3d764baea6e73566032911f9ef1fe082ce5d17
Barry Hylton <blhylton@blhylton.com>
2019-01-25 11:38:39 -0500

    Find Least Common Multiple

---
+++ Pt2Assignment4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Pt2Assignment4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            if (input == null) return;
+            var inputs = input.Split(' ');
+            long[] n = { long.Parse(inputs[0]), long.Parse(inputs[1]) };
+            Console.WriteLine((n[0] * n[1])/EuclidGcd(n[0], n[1]));
+        }
+        
+        //Borrowing from assignment 3
+        private static long EuclidGcd(long a, long b)
+        {
+            while (true)
+            {
+                if (b == 0)
+                {
+                    return a;
+                }
+
+                var rem = a % b;
+                a = b;
+                b = rem;
+            }
+        }
+    }
+}
