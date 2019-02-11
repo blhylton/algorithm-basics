b9adf16b2c3e65d6620a95fa3c933361f158f92b
Barry Hylton <blhylton@blhylton.com>
2019-02-11 03:05:16 -0500

    The goal in this problem is to find the minimum number of coins needed to change the input value (an integer) into coins with denominations 1, 5, and 10.

---
+++ Pt3Assignment1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Pt3Assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            if (input == null) return;
+            var n = ulong.Parse(input);
+            ulong count10 = 0, count5 = 0, count = 0;
+
+            count10 = (n - (n % 10)) / 10;
+            n = n - (count10 * 10);
+            count5 = (n - n % 5) / 5;
+            n = n - (count5 * 5);
+            count = count10 + count5 + n;
+            
+            Console.WriteLine(count);
+        }
+    }
+}
