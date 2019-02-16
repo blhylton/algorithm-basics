2ae7c6cfd8ac42112a17bbcb1b01eef446fa8b47
Barry Hylton <blhylton@blhylton.com>
2019-02-16 11:46:31 -0500

    "You have 𝑛 ads to place on a popular Internet page. For each ad, you know how  much is the advertiser willing to pay for one click on this ad. You have set up 𝑛  slots on your page and estimated the expected number of clicks per day for each  slot. Now, your goal is to distribute the ads among the slots to maximize the  total revenue."

---
+++ Pt3Assignment4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Pt3Assignment3
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var n = long.Parse(input);
+            input = Console.ReadLine();
+            if (input == null) return;
+            var splitInput = input.Split(' ');
+            var a = new long[n];
+            for (var i = 0; i < n; i++)
+            {
+                a[i] = long.Parse(splitInput[i]);
+            }
+
+            Array.Sort<long>(a, new Comparison<long>((i1, i2) => i2.CompareTo(i1)));
+            
+            input = Console.ReadLine();
+            if (input == null) return;
+            splitInput = input.Split(' ');
+            var b = new long[n];
+            for (var i = 0; i < n; i++)
+            {
+                b[i] = long.Parse(splitInput[i]);
+            }
+            
+            Array.Sort<long>(b, new Comparison<long>((i1, i2) => i2.CompareTo(i1)));
+
+            long sum = 0;
+            for (var i = 0; i < n; i++)
+            {
+                sum += (a[i] * b[i]);
+            }
+            
+            Console.WriteLine(sum);
+        }
+    }
+}
