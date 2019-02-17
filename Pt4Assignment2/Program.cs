c9703f5186a9522f509e516a4cdeb4c5e44a09f4
Barry Hylton <blhylton@blhylton.com>
2019-02-17 00:43:39 -0500

    Find if there is a majority in O(n log n)

---
+++ Pt4Assignment2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Pt4Assignment2
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            if (input == null) return;
+            var n = long.Parse(input);
+            input = Console.ReadLine();
+            if (input == null) return;
+            var splitInput = input.Split(' ');
+
+            var arr = new long[n];
+
+            for (var i = 0; i < n; i++)
+            {
+                arr[i] = long.Parse(splitInput[i]);
+            }
+            
+            Array.Sort(arr);
+            
+            Console.WriteLine(FindMajority());
+
+            int FindMajority()
+            {
+                for (var i = 0; i < (long) Math.Ceiling((double) n / 2); i++)
+                {
+                    var j = (long) Math.Floor((double) n / 2) + i;
+                    if (arr[i] == arr[j])
+                    {
+                        return 1;
+                    }
+                }
+
+                return 0;
+            }
+
+            
+        }
+    }
+}
